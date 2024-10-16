45d2b23aa863a084003565fc35acda8297444431
hududed <hudwahab@gmail.com>
2024-10-16 17:54:41 -0600

    init with FB client setup

---
+++ my-app/src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZTpOBUUwXhMm_-Ff86V-jrLxxy6p20T4",
+    authDomain: "fireship-sveltekit-25302.firebaseapp.com",
+    projectId: "fireship-sveltekit-25302",
+    storageBucket: "fireship-sveltekit-25302.appspot.com",
+    messagingSenderId: "201274791831",
+    appId: "1:201274791831:web:7a9fec332efb3d76edd6d2"
+  };
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
